1df4b76a02d05936626a38451b2b7abca09eb6c5
Dhana Shunmugasundram <dhanas@surveymonkey.com>
2021-03-10 22:06:10 -0800

    Finally found the bloody user

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const request = require('request');
 
+const tokens = {
+    'user.read': 'Bearer xoxb-2319467542-1842793841845-E6GwZiCNT3qvHsGfktVa1TJC',
+    'workspace.read': 'Bearer xoxb-2319467542-1842793841845-JlrIkKNKPt2tDt1eDsJFF9tx'
+}
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -21,7 +25,7 @@ app.post('/bonusly', (req, res) => {
     const options = {
         url: `https://slack.com/api/users.profile.get?user=${fromUser}`,
         headers: {
-            'Authorization': 'Bearer xoxb-2319467542-1842793841845-E6GwZiCNT3qvHsGfktVa1TJC'
+            'Authorization': tokens['workspace.read']
         }
     };
 
@@ -29,7 +33,11 @@ app.post('/bonusly', (req, res) => {
         if (!error && response.statusCode == 200) {
             console.log(response);
             console.log(body);
-            findUserInBonusly(body['profile']['email'])
+            var slackEmail = JSON.parse(body)['profile']['email'];
+            if(slackEmail == 'davidg@surveymonkey.com') {
+                slackEmail = 'dgregory@surveymonkey.com';
+            }
+            findUserInBonusly(slackEmail);
         }
         else{
             console.log(error);
@@ -40,13 +48,25 @@ app.post('/bonusly', (req, res) => {
 
     function findUserInBonusly(email){
         const options = {
-            url: `https://bonus.ly/api/v1/users?email?=${email}`,
+            url: `https://bonus.ly/api/v1/users?email=${email}`,
             headers: {
-                'Authorization': 'abbfd9c173805a12738a11f521b1a155'
+                'Authorization': 'Bearer abbfd9c173805a12738a11f521b1a155'
             }
-    };
+        };
 
-        
+        request(options, foundBonuslyUser);     
+    }
+
+    function foundBonuslyUser(error, response, body){
+        if (!error && response.statusCode == 200) {
+            const bonuslyUser = JSON.parse(body);
+            console.log(bonuslyUser.result[0]['email']);
+            response.send("boo");
+        }
+        else{
+            console.log(error);
+            response.send(null);
+        }
     }
 
     request(options, callback);
